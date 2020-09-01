0d366dcb6c588304da6b52c010651167574f11d6
Abhijeet Samanta <a.samanta@dfarminc.com>
2020-09-01 09:14:21 +0530

    add DB

--- db.js
+++ db.js
@@ -15,7 +15,7 @@ const options = {
   connectTimeoutMS: 10000,
 };
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = `mongodb://${Abhi}:${'Abhi@3489'}@${test1.byx8q.mongodb.net/test}:${27017}/${sampledata}?authSource=admin`;
 
 mongoose.connect(url, options).then( function() {
   console.log('MongoDB is connected');
